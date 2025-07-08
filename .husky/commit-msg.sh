7eb010be2fb975af98ff7111a1b862add10e0298
jhlee-codes <jhleezz00@naver.com>
2025-07-08 22:09:14 +0900

    feat: test2

--- .husky/commit-msg.sh
+++ .husky/commit-msg.sh
@@ -1,17 +1,4 @@
-#!/bin/sh
-. "$(dirname "$0")/_/husky.sh"
+#!/usr/bin/env sh.
+"$(dirname -- "$0")/_/husky.sh"
 
-COMMIT_MSG_FILE=$1
-COMMIT_MSG=$(cat "$COMMIT_MSG_FILE")
-
-# 브랜치명에서 이슈 키 추출 (예: MNW-123-기능명 → MNW-123)
-BRANCH_NAME=$(git symbolic-ref --short HEAD)
-ISSUE_KEY=$(echo "$BRANCH_NAME" | grep -oE '[A-Z]+-[0-9]+')
-
-# 이미 이슈 키가 있으면 추가 안 함
-if echo "$COMMIT_MSG" | grep -q "$ISSUE_KEY"; then
-  exit 0
-fi
-
-# 앞에 이슈 키 붙이기
-echo "$ISSUE_KEY $COMMIT_MSG" > "$COMMIT_MSG_FILE"
+npx jira-prepare-commit-msg $1
