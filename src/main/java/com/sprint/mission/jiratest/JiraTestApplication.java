b46496c33adf840ce8dc05861a10e446dd754c76
jhlee-codes <jhleezz00@naver.com>
2025-07-08 21:50:07 +0900

    MNW-1 chore: jira 이슈 close 자동화 설정

---
+++ src/main/java/com/sprint/mission/jiratest/JiraTestApplication.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.jiratest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiraTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sprint/mission/jiratest/JiraTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.jiratest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
